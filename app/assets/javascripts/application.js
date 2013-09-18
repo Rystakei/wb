ce819abf0d54580f36eec1ddc422ef1b1ce4cb24
Krista Williams <rystakei@gmail.com>
2013-09-17 20:11:26 -0400

    Yelp results now show up on the countries' index page.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,68 +14,64 @@
 // //= require jquery_ujs
 // //= require_tree .
 
-// var auth = { 
-//   //
-//   // Update with your auth tokens.
-//   //
-//   consumerKey: "EUcoj33x9x0XScuxAU0Y2w", 
-//   consumerSecret: "-ZPlORm6Q5V_b5OmltDNk2mwGgc",
-//   accessToken: "Ao_8q4xsHf06mQ-SGdhePM0BHIii15uC",
-//   // This example is a proof of concept, for how to use the Yelp v2 API with javascript.
-//   // You wouldn't actually want to expose your access token secret like this in a real application.
-//   accessTokenSecret: "WQGiHuuVgGoMd6h2Bi15EPYLhng",
-//   serviceProvider: { 
-//     signatureMethod: "HMAC-SHA1"
-//   }
-// };
+var auth = { 
+   
+   consumerKey: "EUcoj33x9x0XScuxAU0Y2w", 
+   consumerSecret: "-ZPlORm6Q5V_b5OmltDNk2mwGgc",
+   accessToken: "Ao_8q4xsHf06mQ-SGdhePM0BHIii15uC",
+   accessTokenSecret: "WQGiHuuVgGoMd6h2Bi15EPYLhng",
+   serviceProvider: { 
+     signatureMethod: "HMAC-SHA1"
+   }
+ };
 
-// var terms = 'food';
-// var near = 'San+Francisco';
+ var terms = 'food';
+ var near = 'San+Francisco';
 
-// var accessor = {
-//   consumerSecret: auth.consumerSecret,
-//   tokenSecret: auth.accessTokenSecret
-// };
+ var accessor = {
+   consumerSecret: auth.consumerSecret,
+   tokenSecret: auth.accessTokenSecret
+ };
 
-// parameters = [];
-// parameters.push(['term', terms]);
-// parameters.push(['location', near]);
-// parameters.push(['callback', 'cb']);
-// parameters.push(['oauth_consumer_key', auth.consumerKey]);
-// parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
-// parameters.push(['oauth_token', auth.accessToken]);
-// parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
+ parameters = [];
+ parameters.push(['term', terms]);
+ parameters.push(['location', near]);
+ parameters.push(['callback', 'cb']);
+ parameters.push(['oauth_consumer_key', auth.consumerKey]);
+ parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
+ parameters.push(['oauth_token', auth.accessToken]);
+ parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
 
-// var message = { 
-//   'action': 'http://api.yelp.com/v2/search',
-//   'method': 'GET',
-//   'parameters': parameters 
-// };
+ var message = { 
+   'action': 'http:api.yelp.com/v2/search',
+   'method': 'GET',
+   'parameters': parameters 
+ };
 
-// OAuth.setTimestampAndNonce(message);
-// OAuth.SignatureMethod.sign(message, accessor);
+ OAuth.setTimestampAndNonce(message);
+ OAuth.SignatureMethod.sign(message, accessor);
 
-// var parameterMap = OAuth.getParameterMap(message.parameters);
-// parameterMap.oauth_signature = OAuth.percentEncode(parameterMap.oauth_signature)
-// console.log(parameterMap);
+ var parameterMap = OAuth.getParameterMap(message.parameters);
+ parameterMap.oauth_signature = OAuth.percentEncode(parameterMap.oauth_signature)
+ console.log(parameterMap);
 
-// $.ajax({
-//   'url': message.action,
-//   'data': parameterMap,
-//   'cache': true,
-//   'dataType': 'jsonp',
-//   'jsonpCallback': 'cb',
-//   'success': function(data, textStats, XMLHttpRequest) {
-//     console.log(data);
-//     console.log("Data!" + data[0]);
+ $.ajax({
+   'url': message.action,
+   'data': parameterMap,
+   'cache': true,
+   'dataType': 'jsonp',
+   'jsonpCallback': 'cb',
+   'success': function(data, textStats, XMLHttpRequest) {
+     console.log(data);
+     console.log("Data!" + data[0]);
 
-//       $.each(data["businesses"], function(key, val) {
-//       		$('#results').append('<div class="result result'+key+'">');
-//       		$('.result'+key).append("Name:" + data["businesses"][key]["name"] + "<p>");
-//   			$('.result'+key).append("URL:" + data["businesses"][key]["url"] + "<p>");
-//   			$('.result'+key).append("Phone:" + data["businesses"][key]["phone"] + "<p>");
-//   			$('.result'+key).append("Description:" + data["businesses"][key]["snippet_text"] + "<p>");
-//   			$('.result'+key).append("Type of Cuisine:" + data["businesses"][key]["categories"] + "<p><hr />");
-//   		});
-//   }
-// });
+       $.each(data["businesses"], function(key, val) {
+       		$('#results').append('<div class="result result'+key+'">');
+       		$('.result'+key).append("Name:" + data["businesses"][key]["name"] + "<p>");
+   			$('.result'+key).append("URL:" + data["businesses"][key]["url"] + "<p>");
+   			$('.result'+key).append("Phone:" + data["businesses"][key]["phone"] + "<p>");
+   			$('.result'+key).append("Description:" + data["businesses"][key]["snippet_text"] + "<p>");
+   			$('.result'+key).append("Type of Cuisine:" + data["businesses"][key]["categories"] + "<p><hr />");
+   		});
+   }
+ });
