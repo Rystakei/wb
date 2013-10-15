18bcf31c54a34280bb62d80c34dada2323a0e17e
Krista Williams <rystakei@gmail.com>
2013-10-15 06:45:57 -0400

    Deleted some precompiled css files. Modified the divs slightly on the main page.

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -82,22 +82,4 @@ class CountriesController < ApplicationController
   end
 
 
-  def generate_random_country
-
-    countrieslist = []
-
-    User.last.countries.each do |country|
-      if country.status == 2
-        countrieslist.push(country.name)
-        puts country.name
-      end
-    end
-
-    puts "There are #{countrieslist.length} untried cuisines."
-
-    r = Random.new
-    random_number = r.rand(0..countrieslist.length-1)
-    random_country = countrieslist[random_number]
-    return random_country
-end
 end
